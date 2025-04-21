8a0a153c4cbd76d6542a60d2883f54766f8f145d
Amaury <asasincer@gmail.com>
2025-04-21 14:40:51 +0300

    Initial Commit

---
+++ CatsAPI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CatsAPI
+//
+//  Created by MakOSim on 21.04.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
